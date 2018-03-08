459c68b0f8d630cfff624232315c5a90bf971d23
Raymond Sy <syraymond0320@gmail.com>
2018-03-07 19:53:05 -0800

    initial commit

--- bookkeeping.py
+++ bookkeeping.py
@@ -1,24 +1,15 @@
 import json
+from pprint import pprint
 
 def bookparser():
     file = "/Users/Raymond/PycharmProjects/searchengine/bookkeeping.json"
-    with open(file) as data_file:
-        data = data_file.read()
-        j = json.loads(data)
-        print j['9/86']
-
-def indexloader():
-    file = "/Users/Raymond/PycharmProjects/searchengine/index3.txt"
-    with open(file) as data_file:
-        data = data_file.read()
-        content = data.split(":")
-    print content
+    # with open(file) as data_file:
+    data = json.load(open(file))
 
 def main():
 
     try:
         bookparser()
-        indexloader()
     except:
         print "File not found"
 
--- index.py
+++ index.py
@@ -4,9 +4,12 @@ import re
 from bs4 import BeautifulSoup
 from recordtype import recordtype
 from collections import defaultdict
+import json
+import pymongo
+from pymongo import MongoClient
 
 word_dict = defaultdict(list)
-Record = recordtype('Record', 'folder page TFIDF')
+
 
 def readfile(folder, page):
     file = "/Users/Raymond/PycharmProjects/searchengine/WEBPAGES_CLEAN/{}/{}".format(folder, page)
@@ -18,8 +21,11 @@ def readfile(folder, page):
 def tokenize(contents, f, p):
     global word_dict
     soup = BeautifulSoup(contents, 'html.parser')
+    location = "{}/{}".format(f,p)
+    found = False
     #print (soup.get_text())
-
+    text = soup.get_text()
+    tokens =
     tokens = re.findall("[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\w]+", soup.get_text())
     # Finds all the words matching the regex and puts it in a list
 
@@ -28,17 +34,18 @@ def tokenize(contents, f, p):
             documentList = word_dict.get(token.lower())
             #print documentList
             if documentList:
-                if not any(doc.folder == f and doc.page == p for doc in documentList):
-                    new_record = Record(f, p, 1)
-                    word_dict[token.lower()].append(new_record)
-                else:
-                    for doc in documentList:
-                        if doc.folder == f and doc.page == p:
-                            doc.TFIDF += 1
-
+                for jsonRecord in documentList:
+                    if jsonRecord["docID"] == location:
+                        jsonRecord["Frequency"] = jsonRecord["Frequency"] + 1
+                        found = True
+                        break
+                if not found:
+                    Found = False
+                    jsonObject = {"docID": location, "Frequency": 1}
+                    word_dict[token.lower()].append(jsonObject)  # if it doesnt exist, add to dictionary with frequency of 1
         else:
-            new_record = Record(f, p, 1)
-            word_dict[token.lower()].append(new_record)   # if it doesnt exist, add to dictionary with frequency of 1
+            jsonObject = {"docID": location, "Frequency": 1}
+            word_dict[token.lower()].append(jsonObject)   # if it doesnt exist, add to dictionary with frequency of 1
 
     #print word_dict
     # for word, freq in sorted(word_dict.iteritems(), key=lambda(k, v): (-v, k)):
@@ -47,15 +54,18 @@ def tokenize(contents, f, p):
 
 
 def main():
-    counter = 0
+    client = MongoClient('localhost', 27017)
+    db = client.invertedindex
+    posts = db.index
+
     try:
         # tries to open the file, if any error comes out, jump to except statement
-        for i in range(0, 75):
+        for i in range(0, 1):
             for x in range(0, 500):
                 c = readfile(i,x)
                 tokenize(c, i, x)
-                counter += 1
-                print (counter)
+                # counter += 1
+                # print (counter)
     except:
         print "File not found"
 
@@ -63,9 +73,17 @@ def main():
     values = word_dict.values()
     # for record in zip(keys, values):
     #     print (record[0], ": ", record[1])
-    for record in zip(keys,values):
-        with open("index3.txt", "a") as fl:
-            fl.write(record[0] + ": " + str(record[1]) + "\n")
+    # for record in zip(keys,values):
+    #     with open("index5.txt", "a") as fl:
+    #         fl.write(record[0] + ": " + str(record[1]) + "\n")
+    for record in zip(keys, values):
+        post_data = {'token': record[0],
+                     'records': record[1]}
+        result = posts.insert_one(post_data)
+
+    bills_post = posts.find_one({'token': 'this'})
+    for item in bills_post['records']:
+        print(item)
 
 if __name__ == "__main__":
     main()
